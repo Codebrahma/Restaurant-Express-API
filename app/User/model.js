2dbe551871b7dec96c8fcd0c6bb471937bc7b7ac
prasanna1211 <prasanna@codebrahma.com>
2018-06-25 21:01:30 +0530

    added and altered model

--- app/User/model.js
+++ app/User/model.js
@@ -19,6 +19,11 @@ var UserSchema = new Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user',
+  }
 });
 
 module.exports = mongoose.model('User', UserSchema);
